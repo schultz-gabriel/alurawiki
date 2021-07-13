2b4af2882f4ccb4e68acc6b2c05dc1acae5bab22
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2021-07-12 22:00:49 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
   }
+
+  #__next{
+    display: flex;
+    flex-direction:column;
+    min-height: 100vh;
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
+
 `
+
 
 const theme = {
   colors: {
---
+++ src/lib/AlurakutCommons.js
@@ -0,0 +1,293 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import NextLink from 'next/link';
+import AlurakutLoginScreen from '../components/LoginScreen'
+
+const BASE_URL = 'http://alurakut.vercel.app/';
+const v = '1';
+
+
+function Link({ href, children, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>
+        {children}
+      </a>
+    </NextLink>
+  )
+}
+
+// ================================================================================================================
+// Menu
+// ================================================================================================================
+export function AlurakutMenu({ githubUser }) {
+  const [isMenuOpen, setMenuState] = React.useState(false);
+  return (
+    <AlurakutMenu.Wrapper isMenuOpen={isMenuOpen}>
+      <div className="container">
+        <AlurakutMenu.Logo src={`${BASE_URL}/logo.svg`} />
+
+        <nav style={{ flex: 1 }}>
+          {[{ name: 'Inicio', slug: '/'}, {name: 'Amigos', slug: '/amigos'}, {name: 'Comunidades', slug: '/comunidades'}].map((menuItem) => (
+            <Link key={`key__${menuItem.name.toLocaleLowerCase()}`} href={`${menuItem.slug.toLocaleLowerCase()}`}>
+              {menuItem.name}
+            </Link>
+          ))}
+        </nav>
+
+        <nav>
+          <a href={`/logout`}>
+            Sair
+          </a>
+          <div>
+            <input placeholder="Pesquisar no Orkut" />
+          </div>
+        </nav>
+
+        <button onClick={() => setMenuState(!isMenuOpen)}>
+          {isMenuOpen && <img src={`${BASE_URL}/icons/menu-open.svg?v=${v}`} />}
+          {!isMenuOpen && <img src={`${BASE_URL}/icons/menu-closed.svg?v=${v}`} />}
+        </button>
+      </div>
+      <AlurakutMenuProfileSidebar githubUser={githubUser} />
+    </AlurakutMenu.Wrapper>
+  )
+}
+AlurakutMenu.Wrapper = styled.header`
+  width: 100%;
+  background-color: #308BC5;
+  .alurakutMenuProfileSidebar {
+    background: white;
+    position: fixed;
+    z-index: 100;
+    padding: 46px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 48px;
+    transition: .3s;
+    pointer-events: ${({ isMenuOpen }) => isMenuOpen ? 'all' : 'none'};
+    opacity: ${({ isMenuOpen }) => isMenuOpen ? '1' : '0'};
+    transform: ${({ isMenuOpen }) => isMenuOpen ? 'translateY(0)' : 'translateY(calc(-100% - 48px))'};
+    @media(min-width: 860px) {
+      display: none;
+    }
+    > div {
+      max-width: 400px;
+      margin: auto;
+    }
+    a {
+      font-size: 18px;
+    }
+    .boxLink {
+      font-size: 18px;
+      color: #2E7BB4;
+      -webkit-text-decoration: none;
+      text-decoration: none;
+      font-weight: 800;
+    }
+    hr {
+      margin-top: 12px;
+      margin-bottom: 8px;
+      border-color: transparent;
+      border-bottom-color: #ECF2FA;
+    }
+  }
+  .container {
+    background-color: #308BC5;
+    padding: 7px 16px;
+    max-width: 1110px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    z-index: 101;
+    @media(min-width: 860px) {
+      justify-content: flex-start;
+    }
+    button {
+      border: 0;
+      background: transparent;
+      align-self: center;
+      display: inline-block;
+      @media(min-width: 860px) {
+        display: none;
+      }
+    }
+    nav {
+      display: none;
+      @media(min-width: 860px) {
+        display: flex;
+      }
+      a {
+        font-size: 12px;
+        color: white;
+        padding: 10px 16px;
+        position: relative;
+        text-decoration: none;
+        &:after {
+          content: " ";
+          background-color: #5292C1;
+          display: block;
+          position: absolute;
+          width: 1px;
+          height: 12px;
+          margin: auto;
+          left: 0;
+          top: 0;
+          bottom: 0;
+        }
+      }
+    }
+    input {
+      color: #ffffff;
+      background: #5579A1;
+      padding: 10px 42px;
+      border: 0;
+      background-image: url(${`${BASE_URL}/icons/search.svg`});
+      background-position: 15px center;
+      background-repeat: no-repeat;
+      border-radius: 1000px;
+      font-size: 12px;
+      ::placeholder {
+        color: #ffffff;
+        opacity: 1;
+      }
+    } 
+  }
+`;
+AlurakutMenu.Logo = styled.img`
+  background-color: #ffffff;
+  padding: 9px 14px;
+  border-radius: 1000px;
+  height: 34px;
+`;
+
+function AlurakutMenuProfileSidebar({ githubUser }) {
+  return (
+    <div className="alurakutMenuProfileSidebar">
+      <div>
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+        <hr />
+        <p>
+          <a className="boxLink" href={`/user/${githubUser}`}>
+            @{githubUser}
+          </a>
+        </p>
+        <hr />
+
+        <AlurakutProfileSidebarMenuDefault />
+      </div>
+    </div>
+  )
+}
+
+// ================================================================================================================
+// AlurakutProfileSidebarMenuDefault
+// ================================================================================================================
+export function AlurakutProfileSidebarMenuDefault() {
+  return (
+    <AlurakutProfileSidebarMenuDefault.Wrapper>
+      <nav>
+        <a href="/">
+          <img src={`${BASE_URL}/icons/user.svg`} />
+            Perfil
+          </a>
+        <a href="/">
+          <img src={`${BASE_URL}/icons/book.svg`} />
+            Recados
+          </a>
+        <a href="/">
+          <img src={`${BASE_URL}/icons/camera.svg`} />
+            Fotos
+          </a>
+        <a href="/">
+          <img src={`${BASE_URL}/icons/sun.svg`} />
+            Depoimentos
+          </a>
+      </nav>
+      <hr />
+      <nav>
+        <a href="/">
+          <img src={`${BASE_URL}/icons/plus.svg`} />
+            GitHub Trends
+          </a>
+        <a href="/logout">
+          <img src={`${BASE_URL}//icons/logout.svg`} />
+            Sair
+          </a>
+      </nav>
+    </AlurakutProfileSidebarMenuDefault.Wrapper>
+  )
+}
+AlurakutProfileSidebarMenuDefault.Wrapper = styled.div`
+  a {
+    font-size: 12px;
+    color: #2E7BB4;
+    margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    text-decoration: none;
+    img {
+      width: 16px;
+      height: 16px;
+      margin-right: 5px; 
+    }
+  }
+`;
+
+// ================================================================================================================
+// OrkutNostalgicIconSet
+// ================================================================================================================
+
+
+// ================================================================================================================
+// Login Page
+// ================================================================================================================
+
+// ================================================================================================================
+// Reset Styles
+// ================================================================================================================
+export const AlurakutStyles = css`
+  *::-webkit-scrollbar {
+    width: 8px;
+  }
+  *::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+  }
+  *::-webkit-scrollbar-thumb {
+    background: #888; 
+    border-radius: 10px;
+  }
+  *::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+  }
+  a,
+  button {
+    cursor: pointer;
+    transition: .3s;
+    outline: 0;
+    &:hover,
+    &:focus {
+      opacity: .8;
+    }
+    &:disabled {
+      cursor: not-allowed;
+      opacity: .5;
+    }
+  }
+  input {
+    transition: .3s;
+    outline: 0;
+    &:disabled {
+      cursor: not-allowed;
+      opacity: .5;
+    }
+    &:hover,
+    &:focus {
+      box-shadow: 0px 0px 5px #33333357;
+    }
+  }
+  ${AlurakutLoginScreen}
+`; 
